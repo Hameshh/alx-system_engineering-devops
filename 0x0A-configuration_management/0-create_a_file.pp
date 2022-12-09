19cbb14ca07651ccdfe19342ed17defc2fd8e061
Hameshh <jossy21w@yahoo.com>
2022-12-09 11:53:41 -0800

    fixxx

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,8 @@
-# Create a file in /tmp
-file {'/tmp/school':
+# creates a file in the /tmp directory
+file { '/tmp/school':
+  ensure  => 'file',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love Puppet'
+  content => 'I love Puppet',
+}
