e6876034f494b729454d8642538ba97398ffd7f6
Hameshh <jossy21w@yahoo.com>
2022-12-09 10:35:19 -0800

    fixx

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,6 @@
-# This Puppet manifest creates a file in /tmp
-
-file { 'holberton':
-  ensure  => file,
-  path    => '/tmp/holberton',
+# Create a file in /tmp
+file {'/tmp/school':
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
   content => 'I love Puppet'
-}
